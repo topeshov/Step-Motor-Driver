d5de4f110bdf328a1d826d5b252d16e44d65fcf9
topeshov <44240938+topeshov@users.noreply.github.com>
2018-12-04 17:12:08 +0100

    Add files via upload

---
+++ Arduino/Arduino.ino
@@ -0,0 +1,138 @@
+#include <Wire.h>
+
+int Pin1 = 10;
+int Pin2 = 11;
+int Pin3 = 12;
+int Pin4 = 13;
+
+int Step = 9;
+int Rotation = 0;
+int FullRotation = 4096;  // Ilość kroków wymagana do wykonania pełnego obrotu
+int CurrentRotation = 0;
+int RememberRotation = 0;
+boolean Direction = false; // false - zgodnie z ruchem wskazówek zegara, true = przeciwnie
+
+void setup()
+{
+  Wire.begin(1);                // Nadanie adresu I2C
+  Wire.onReceive(receiveEvent); // Zainicjowanie funkcji, która wykona się gdy Arduino otrzyma dane
+  Serial.begin(9600);           // Szybkość transmisji danych
+
+  pinMode(Pin1, OUTPUT);        
+  pinMode(Pin2, OUTPUT);
+  pinMode(Pin3, OUTPUT);
+  pinMode(Pin4, OUTPUT);
+}
+
+void loop()
+{
+  switch (Step) // Pierwsze 8 case'ów wymagane są do wykonania kroku
+  {             // Ostatni case to zatrzymanie silnika
+    case 0:
+      digitalWrite(Pin1, LOW);
+      digitalWrite(Pin2, LOW);
+      digitalWrite(Pin3, LOW);
+      digitalWrite(Pin4, HIGH);
+      break;
+
+    case 1:
+      digitalWrite(Pin1, LOW);
+      digitalWrite(Pin2, LOW);
+      digitalWrite(Pin3, HIGH);
+      digitalWrite(Pin4, HIGH);
+      break;
+
+    case 2:
+      digitalWrite(Pin1, LOW);
+      digitalWrite(Pin2, LOW);
+      digitalWrite(Pin3, HIGH);
+      digitalWrite(Pin4, LOW);
+      break;
+
+    case 3:
+      digitalWrite(Pin1, LOW);
+      digitalWrite(Pin2, HIGH);
+      digitalWrite(Pin3, HIGH);
+      digitalWrite(Pin4, LOW);
+      break;
+
+    case 4:
+      digitalWrite(Pin1, LOW);
+      digitalWrite(Pin2, HIGH);
+      digitalWrite(Pin3, LOW);
+      digitalWrite(Pin4, LOW);
+      break;
+
+    case 5:
+      digitalWrite(Pin1, HIGH);
+      digitalWrite(Pin2, HIGH);
+      digitalWrite(Pin3, LOW);
+      digitalWrite(Pin4, LOW);
+      break;
+
+    case 6:
+      digitalWrite(Pin1, HIGH);
+      digitalWrite(Pin2, LOW);
+      digitalWrite(Pin3, LOW);
+      digitalWrite(Pin4, LOW);
+      break;
+
+    case 7:
+      digitalWrite(Pin1, HIGH);
+      digitalWrite(Pin2, LOW);
+      digitalWrite(Pin3, LOW);
+      digitalWrite(Pin4, HIGH);
+      break;
+
+    case 9:
+      digitalWrite(Pin1, LOW);
+      digitalWrite(Pin2, LOW);
+      digitalWrite(Pin3, LOW);
+      digitalWrite(Pin4, LOW);
+      break;
+  }
+  if (Step != 9)
+  {
+    if (Direction)
+      Step++;
+    else
+      Step--;
+
+    if (Step == 8)
+      Step = 0;
+
+    if (Step < 0)
+      Step = 7;
+  }
+
+  CurrentRotation--;
+  if (CurrentRotation <= 0)
+    Step = 9;
+  delay(1);
+}
+
+void receiveEvent(int howMany)
+{
+  int x = Wire.read();    // Odczytanie otrzymanej wartości
+  if (x == 255)           // Jeśli wartość ta = 255 następuje zmiana kierunku
+    Direction = !Direction;
+  if (x > 0 && x < 255)   // Jeśli wartość ta zawiera się w podanym przedziale, silnik wykona tyle obrotów
+  {
+    Rotation = x;
+    CurrentRotation = FullRotation * Rotation;
+    if (Step == 9)
+      Step = 0;
+  }
+  if (x == 0)             // Jeśli wartość = 0, silnik ruszy lub zatrzyma się
+  { 
+    if (Step == 9)
+    {
+      CurrentRotation = FullRotation * Rotation;      
+      Step = 0;
+    }
+    else
+      Step = 9;
+  }
+
+  Serial.println(x);        
+}
---
+++ I2C Motor Driver/I2C Motor Driver/Sterownik silnika.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MCP2221;
+
+namespace I2C_Motor_Driver
+{
+    public partial class Form1 : Form
+    {
+        MCP2221.MchpUsbI2c UsbI2c = new MchpUsbI2c();
+        bool isConnected;
+        byte[] Stop = { 0 };
+        byte[] Direction = { 255 };
+        byte Adres = 2;
+        int isNumber = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Szukaj_Click(object sender, EventArgs e)
+        {
+            string Connected;
+            
+            isConnected = UsbI2c.Settings.GetConnectionStatus();
+            
+            if (isConnected == true)
+            {
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+
+                Connected = "Układ MCP2221 został podłączony.\n";
+                
+                int devCount = UsbI2c.Management.GetDevCount();
+                string AmountMCP = "Podłączony jest " + devCount.ToString() + " układ MCP2221 do komputera PC.\n";
+                UsbI2c.Management.SelectDev(0);
+                
+                string usbDescriptor = UsbI2c.Settings.GetUsbStringDescriptor();
+                string Descriptor = "Deskryptor USB: " + usbDescriptor + "\n";
+                
+                richTextBox1.Text = Connected + AmountMCP + Descriptor;
+            }
+            else
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                Connected = "Układ MCP2221 jest niepodłączony.\n";
+                richTextBox1.Text = Connected;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (UsbI2c.Functions.WriteI2cData(Adres, Stop, 1, 9600) == 0)
+                richTextBox1.Text += "OK - Stan zmieniony\n";
+            else
+                richTextBox1.Text += "Błąd połączenia\n";
+            UsbI2c.Functions.StopI2cDataTransfer();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (UsbI2c.Functions.WriteI2cData(Adres, Direction, 1, 9600) == 0)
+                richTextBox1.Text += "OK - Kierunek zmieniony\n";
+            else
+                richTextBox1.Text += "Błąd połączenia\n";
+            UsbI2c.Functions.StopI2cDataTransfer();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int InputRot = Int32.Parse(textBox1.Text);
+            if (InputRot > 0 && InputRot < 255)
+            {
+                byte RotationByte = Convert.ToByte(InputRot);
+                byte[] Rotation = { RotationByte };
+                if (UsbI2c.Functions.WriteI2cData(Adres, Rotation, 1, 9600) == 0)
+                {
+                    UsbI2c.Functions.StopI2cDataTransfer();
+                    richTextBox1.Text += "Ustawiono liczbę obrotów: " + InputRot + "\n";
+                }
+                else
+                    richTextBox1.Text += "Błąd połączenia\n";
+            }
+            else
+                MessageBox.Show("Nieprawidłowa ilość obrotów!", "Błąd!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+    }
+}
+
